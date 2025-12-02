e8ffcab7e3c097c69144a78c94f0ac1122fb3243
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2025-12-02 12:01:47 +0530

    Update Python_OOPS.py

--- Python_OOPS.py
+++ Python_OOPS.py
@@ -134,3 +134,30 @@ s1.hello()
 
 
 
+
+
+
+
+
+# ABSTRACTION........................
+
+
+
+
+
+class Car:
+    def __init__(self):
+        self.acc=False
+        self.brk=False
+        self.clutch=False
+    def start(self):
+        self.clutch=True
+        self.acc=True
+        print('car started....')
+        
+car1=Car()
+car1.start()
+        
+        
+        
+  
