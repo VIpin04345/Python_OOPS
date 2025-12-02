5e9bca1b2c6506aa057c6c5826af74bf21893906
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2025-12-02 12:02:18 +0530

    Update Python_OOPS.py

--- Python_OOPS.py
+++ Python_OOPS.py
@@ -158,6 +158,33 @@ class Car:
 car1=Car()
 car1.start()
         
+
+
+
         
+        
+        
+# ENCAPSULATION...................
+
+class Account:
+    def __init__(self,bal,acc):
+        self.balance=bal
+        self.account_no=acc
+        
+    def debit(self,ammount):
+        self.balance-=ammount
+        print(ammount,"debited",self.get_balance())
+        
+    def credit(self,ammount):
+        self.balance+=ammount
+        print(ammount,"added",self.get_balance())
+        
+    def get_balance(self):
+        return self.balance
+        
+    
+acc1=Account(10000,12345)
+acc1.debit(5000)
+acc1.credit(12000)
         
   
