87aaac91a6e6df9b7e31998be10035d69ff94860
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-01-18 04:38:09 +0530

    Update Python_OOPS.py

--- Python_OOPS.py
+++ Python_OOPS.py
@@ -297,3 +297,15 @@ class Calculator:
 c = Calculator()
 print(c.add(10, 20))
 
+
+class Animal:
+    def sound(self):
+        print("Animal sound")
+
+class Dog(Animal):
+    def bark(self):
+        print("Dog barks")
+
+d = Dog()
+d.sound()
+d.bark()
