4c265767fb923a06a0c218c731b4028259c73aae
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-01-20 06:29:43 +0530

    Update Python_OOPS.py

--- Python_OOPS.py
+++ Python_OOPS.py
@@ -522,3 +522,21 @@ class FileManager:
 
 with FileManager():
     print("Working with file")
+
+
+
+class Engine:
+    def start(self):
+        print("Engine started")
+
+class Car:
+    def __init__(self):
+        self.engine = Engine()
+
+    def drive(self):
+        self.engine.start()
+        print("Car is moving")
+
+c = Car()
+c.drive()
+
