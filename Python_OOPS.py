a5efc89031f7b5a21676fa907b6faa03046ae885
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-01-20 06:25:12 +0530

    Update Python_OOPS.py

--- Python_OOPS.py
+++ Python_OOPS.py
@@ -443,3 +443,24 @@ class Child(Father, Mother):
 
 c = Child()
 c.skill()
+
+
+class Animal:
+    def sound(self):
+        print("Animal makes sound")
+
+class Dog(Animal):
+    def sound(self):
+        print("Dog barks")
+
+class Cat(Animal):
+    def sound(self):
+        print("Cat meows")
+
+a = Animal()
+d = Dog()
+c = Cat()
+
+a.sound()
+d.sound()
+c.sound()
