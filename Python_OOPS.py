a54e78504419fcb767cce40650c1eb83fdb2929f
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-03-14 15:18:34 +0530

    Update Python_OOPS.py

--- Python_OOPS.py
+++ Python_OOPS.py
@@ -10,7 +10,15 @@ s.ram()
 
 
 
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
+s1 = Student("Rahul", 21)
+
+print(s1.name)
+print(s1.age)
 
 class Some():
     def loop(self):
