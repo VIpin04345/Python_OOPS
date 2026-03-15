cfc9123a57726699297be241e6d9cfdd7fd707ae
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-03-15 14:59:50 +0530

    Update Python_OOPS.py

--- Python_OOPS.py
+++ Python_OOPS.py
@@ -222,7 +222,22 @@ s2=Student('kishan',99)
 print(s1.name,s1.marks)
 
 
+class A:
+    def showA(self):
+        print("Class A")
 
+class B(A):
+    def showB(self):
+        print("Class B")
+
+class C(B):
+    def showC(self):
+        print("Class C")
+
+obj = C()
+obj.showA()
+obj.showB()
+obj.showC()
 
 
 
