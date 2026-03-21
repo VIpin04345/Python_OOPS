6d750d86c194dd51fed00714a1c6ac4755df4fa4
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-03-21 10:57:52 +0530

    Update Python_OOPS.py

--- Python_OOPS.py
+++ Python_OOPS.py
@@ -730,6 +730,26 @@ class Employee:
 e = Employee(40000)
 print(e.get_salary())
 
+class BankAccount:
+    def __init__(self, balance):
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+
+    def withdraw(self, amount):
+        if amount <= self.balance:
+            self.balance -= amount
+        else:
+            print("Insufficient balance")
+
+    def show_balance(self):
+        print("Balance:", self.balance)
+
+acc = BankAccount(5000)
+acc.deposit(2000)
+acc.withdraw(3000)
+acc.show_balance()
 
 
 def f1():
