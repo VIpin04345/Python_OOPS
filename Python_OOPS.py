3a3052cccb4eaf333fd8bbd6971b6c18d6c186b0
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-04-27 23:11:02 +0530

    Update Python_OOPS.py

--- Python_OOPS.py
+++ Python_OOPS.py
@@ -8,6 +8,16 @@ class hello:
 s=hello()
 s.ram()
 
+class hello:
+    def ram(self):
+        a=12
+        b=13
+        c=a+b
+        print(c)
+
+s=hello()
+s.ram()
+
 class Parent:
     def house(self):
         print("Parent house")
