a44a57120ce9e1ab2765899f1d8a410394687f0f
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-05-20 07:43:58 +0530

    Update Advance OOps.py

--- Advance OOps.py
+++ Advance OOps.py
@@ -6,3 +6,14 @@ class Myclass(object):
 
 x=Myclass()
 x.display()
+
+class Mobile:
+    def __init__(self):
+        self.model='RealmeX'
+    def display(self):
+        print("model:",self.model)
+x=Mobile()
+x.display()
+x.model='OnePlus11R'
+print(x.model)
+x.display()
