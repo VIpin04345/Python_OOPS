4c5318705ba90056253708e871646043ed492062
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-05-23 07:17:53 +0530

    Update Advance OOps.py

--- Advance OOps.py
+++ Advance OOps.py
@@ -266,3 +266,17 @@ class Mobile:
         print("Model:",self.moodel,'And Price:',self.price)
 realme=Mobile('RealmeX')
 realme.show(1000)
+
+
+
+class Mobile:
+    def __init__(self):
+        self.model = "RealmeX"
+
+    def get_model(self):
+        return self.model
+
+
+realme = Mobile()
+m = realme.get_model()
+print(m)
